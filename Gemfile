0a44772db7e8f62f20c450820b0c900991b1b6ce
Sebastian Almeida <18198367+sebasalmeidam@users.noreply.github.com>
2020-12-24 00:38:24 -0500

    installed graphql

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'pry-rails'
 #react-rails
 gem 'react-rails'
 
+#graphql
+gem 'graphql'
+gem 'graphiql-rails', group: :development
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   devise_for :users, path_names: { sign_in: 'sign_in', sign_up: 'sign_up' }
 
   post 'validate_students', to: 'students#validate_students'
