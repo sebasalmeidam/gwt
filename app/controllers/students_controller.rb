6cfc99d7205c82637c3f4ae6180130800ea32739
Sebastian Almeida <18198367+sebasalmeidam@users.noreply.github.com>
2020-12-24 07:00:31 -0500

    load data in batches

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,9 @@ class StudentsController < ApplicationController
 
     @new_students = @students.map{|user| {torre_username: user, organization: params[:organization], created_at: DateTime.now, updated_at: DateTime.now}}
     unless @new_students.empty?
-      Student.insert_all(@new_students)
+      @new_students.in_groups_of(50, false) do |group|
+        Student.insert_all(group)
+      end
       system "rake get_users_genome USERNAMES=#{@students.join(',')} &"
       render json: {status: 200, message: 'Processing'} and return
     end
