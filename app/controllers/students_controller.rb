611bf517c06f60341ff804be5bb8ddd03528307d
Sebastian Almeida <18198367+sebasalmeidam@users.noreply.github.com>
2020-12-23 19:04:10 -0500

    student model and call to insert students from organization

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,8 +5,10 @@ class StudentsController < ApplicationController
     @current_students = Student.where(torre_username: @users ).map(&:torre_username)
     @students = @users - @current_students
 
-    @new_students = @users.map{|user| {torre_username: user, created_at: DateTime.now, updated_at: DateTime.now}}
-    Student.insert_all(@new_students)
+    @new_students = @students.map{|user| {torre_username: user, created_at: DateTime.now, updated_at: DateTime.now}}
+    unless @new_students.empty?
+      Student.insert_all(@new_students)      
+    end
     
     render json: {status: 200, mensaje: 'Received'} and return
   end
